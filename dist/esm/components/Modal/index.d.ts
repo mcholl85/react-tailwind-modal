0912b6dfd31fdb0fd7d166944f5c69f6f5d76dc8
Matthieu Chollet <matthieuchollet@MacBook-Pro-de-Matthieu.local>
2022-08-31 23:58:45 +0200

    fix: change files components

--- dist/esm/components/Modal/index.d.ts
+++
@@ -1,8 +0,0 @@
-/// <reference types="react" />
-declare type ModalProps = {
-    title: string;
-    content: string;
-    onClick: () => void;
-};
-export default function Modal({ title, content, onClick }: ModalProps): JSX.Element;
-export {};
