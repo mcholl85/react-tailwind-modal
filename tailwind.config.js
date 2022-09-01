ad414d3dbb9c0825120cf2b55c9da8b9a09d3255
Matthieu Chollet <matthieuchollet@MacBook-Pro-de-Matthieu.local>
2022-09-01 11:59:11 +0200

    Initial pages commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./dist/*.{html,js,ts,jsx,tsx}'],
+  content: [
+    './dist/**/*.{html,js,ts,jsx,tsx}',
+    './src/**/*.{html,js,ts,jsx,tsx}',
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
